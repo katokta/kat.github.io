51c9f2fcb1862cfbc281cec7445993e7e5c93232
katokta <57477107+katokta@users.noreply.github.com>
2019-11-08 14:02:50 +0700

    Katherine Oktaviani

---
+++ DesIn/js/main.js
@@ -0,0 +1,29 @@
+function myFunction(imgs) {
+  var expandImg = document.getElementById("expandedImg");
+  var imgText = document.getElementById("imgtext");
+  expandImg.src = imgs.src;
+  imgText.innerHTML = imgs.alt;
+  expandImg.parentElement.style.display = "block";
+}
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+$('#va-accordion').vaccordion({
+	expandedHeight	: 350,
+	animSpeed		: 400,
+	animOpacity		: 0.7,
+	visibleSlices	: 2
+});
